d1be7af130cba1c0fc3b0767cdf58befb86a4df6
Brady Douthit <brady.douthit99@gmail.com>
2020-06-15 16:20:39 -0400

    init setup

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Next Budget</h1>
+            </div>
+        )
+    }
+}
+
+export default Home;
